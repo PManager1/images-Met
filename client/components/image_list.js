bf7983f6d1ad3836e1b91c6b4eec51422371aab0
jpca999 <jpca999@gmail.com>
2017-04-22 20:27:46 -0400

    -

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -5,7 +5,9 @@ const IMAGES = [
   { title: 'Pen', link: 'https://dummyimage.com/600x400' },
   { title: 'Pine Tree', link: 'https://dummyimage.com/600x400' },
   { title: 'Mug', link: 'https://dummyimage.com/600x400' }
-]
+];
+
+
 
 const ImageList = () => {
   const RenderedImages = IMAGES.map(function (image) {
@@ -13,7 +15,7 @@ const ImageList = () => {
   });
 
         return (
-          <ul>
+          <ul className="media-list list-group">
            {RenderedImages}
           </ul>
         );
