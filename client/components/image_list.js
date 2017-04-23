6416d883410cdfd4d84f54d20feabf67e40f6cd3
jpca999 <jpca999@gmail.com>
2017-04-22 20:32:47 -0400

    20

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -8,10 +8,9 @@ const IMAGES = [
 ];
 
 
-
 const ImageList = () => {
-  const RenderedImages = IMAGES.map(function (image) {
-      return  <ImageDetail image={image}/>
+  const RenderedImages = IMAGES.map( (image) => {
+      return  <ImageDetail key={image.title} image={ image }/>
   });
 
         return (
